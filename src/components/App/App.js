de1a5588bee890c9962cff4e557284fd8c6bf6ed
John Costa <johncosta027@gmail.com>
2021-06-23 00:25:54 +0100

    Added sync function to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, userEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import AppBar from '../AppBar';
@@ -6,7 +6,31 @@ import ProtectedRoute from '../../auth/protected-route';
 import Profile from '../Profile';
 import ExternalApi from '../views/ExternalAPI';
 
+/**
+ * App component
+ *  * App calls /api/sync method to register, or update user on database
+ */
+
 const App = () => {
+
+	useEffect(() => {
+		sync();
+	}, []);
+
+	const { getAccessTokenSilently } = useAuth0();
+	const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+	const sync = async () => {
+		try {
+			const token = await getAccessTokenSilently();
+			await fetch(`${serverUrl}/api/sync`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				}
+			});
+		} catch (error) {}
+	};
+
 	return (
 		<div id="app">
 			<AppBar />
@@ -14,7 +38,10 @@ const App = () => {
 				<Switch>
 					<Route path="/" exact />
 					<ProtectedRoute path="/profile" component={Profile} />
-					<ProtectedRoute path="/external-api" component={ExternalApi} />
+					<ProtectedRoute
+						path="/external-api"
+						component={ExternalApi}
+					/>
 				</Switch>
 			</div>
 		</div>
