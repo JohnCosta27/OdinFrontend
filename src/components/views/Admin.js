5c25bcf6dbdc9e46185bad07e638fb44680de64a
John Costa <johncosta027@gmail.com>
2021-07-15 14:54:45 +0100

    Added request for users in admin dashboard

--- src/components/views/Admin.js
+++ src/components/views/Admin.js
@@ -4,9 +4,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 import ViewWrapper from '../general/ViewWrapper';
 import CreateSubject from '../subjects/CreateSubject';
 import CreateSubjectPoints from '../subjects/CreateSubjectPoints'
+import FileUpload from '../general/FileUpload';
 
 const Admin = () => {
 	const { getAccessTokenSilently } = useAuth0();
+	const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 	const [allowed, setAllowed] = useState(false);
 
@@ -19,11 +21,23 @@ const Admin = () => {
 			const jwt = jwtDecode(token);
 			if (jwt.permissions.includes('role:admin')) {
                 setAllowed(true);
+				getAllUsers();
 			} else {
                 window.location.href = '/dashboard';
             }
 		});
 	};
+
+	const getAllUsers = async () => {
+		const token = await getAccessTokenSilently();
+		const data = await fetch(`${serverUrl}/users/getall`, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		const responseData = await data.json();
+		console.log(responseData);
+	}
 
 	if (!allowed) {
 		return null;
@@ -32,6 +46,7 @@ const Admin = () => {
 			<ViewWrapper>
 				<CreateSubject />
 				<CreateSubjectPoints />
+				<FileUpload />
 			</ViewWrapper>
 		);
 	}
