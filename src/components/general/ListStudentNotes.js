96fdd01737b08f0588f96314778fa3dfca917cf5
John Costa <johncosta027@gmail.com>
2021-07-30 16:43:58 +0100

    Finished your notes section of dashboard, I think we're done

--- src/components/general/ListStudentNotes.js
+++ src/components/general/ListStudentNotes.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import ListStudentNoteSingle from './ListStudentNoteSingle';
 
 const ListStudentNotes = () => {
 	const serverUrl = process.env.REACT_APP_SERVER_URL;
@@ -23,41 +22,21 @@ const ListStudentNotes = () => {
 			},
 		});
 		const responseData = await data.json();
-		console.log(responseData);
 		setUsernotes(responseData);
 		setLoading(false);
 	};
 
-	const getDateString = (dateString) => {
-		const date = new Date(dateString);
-		let dd = date.getDate();
-		let mm = date.getMonth() + 1;
-		const yyyy = date.getFullYear();
-		if (dd < 10) dd = '0' + dd;
-		if (mm < 10) mm = '0' + mm;
-		return dd + '/' + mm + '/' + yyyy;
-	};
-
-    const getContent = (content) => {
-        if (content.length > 40) {
-            return content.substring(0, 37) + "...";
-        }
-        return content;
-    }
-
 	return (
 		<List>
 			{usernotes.map((note) => (
-				<ListItem key={note.id}>
-					<ListItemText
-						primary={note.text_notes.title}
-						secondary={getContent(note.text_notes.content)}
-					/>
-					<ListItemText
-						align="right"
-						primary={getDateString(note.text_notes.datetime)}
-					/>
-				</ListItem>
+				<div key={note.id}>
+				<ListStudentNoteSingle
+					title={note.title}
+					content={note.content}
+					datetime={note.datetime}
+					pointid={note.pointid}
+				/>
+				</div>
 			))}
 		</List>
 	);
